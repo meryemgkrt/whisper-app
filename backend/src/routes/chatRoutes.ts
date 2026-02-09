244e959f083f401380437127e0933b3428b8b012
meryemgkrt <meryemkurt824@gmail.com>
2026-02-10 00:14:25 +0300

    Update chat routes

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
+
 import { Router } from "express";
 
 const router = Router();
 
-=======
-import {Router} from "express";
-
-const router = Router();
-
-
->>>>>>> 3138cd5 (backende update)
 export default router;
+
