8166b2965f614aaafdcdbf500145eb8286dac45e
meryemgkrt <meryemkurt824@gmail.com>
2026-02-11 23:40:45 +0300

    fix: correct MongoDB URI variable name and update User model avatar property to be required

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const mongoUri= process.env.ONGODB_URI;
+        const mongoUri= process.env.MONGODB_URI;
         if(!mongoUri){
             throw new Error("MONGODB_URI is not defined in environment variables");
         }
