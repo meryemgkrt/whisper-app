f91cd9a8782d4237ee83d95b135990def74e6386
meryemgkrt <meryemkurt824@gmail.com>
2026-02-15 00:35:05 +0300

    feat: integrate Clerk for authentication and user management
    - Added Clerk dependencies to backend and web package.json files.
    - Implemented Clerk middleware in backend app.
    - Created auth middleware for protected routes.
    - Developed auth controllers for user retrieval and callback handling.
    - Updated auth routes to include new endpoints.
    - Modified frontend App component to utilize Clerk's authentication UI.
    - Wrapped App component with ClerkProvider in main entry file.
    - Added global types for Clerk in backend.
    - Removed unused authControllers file.

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,6 +1,10 @@
 import {Router} from "express";
+import { protecRoute } from "../middleware/auth";
+import { authCallback, getMe } from "../controllers/authControllers";
 
 const router = Router();
 
+router.get("/me", protecRoute, getMe);
+router.post("/callback", authCallback)
 
 export default router;
