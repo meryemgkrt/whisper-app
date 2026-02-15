f29897607b51711fe8756511c225780e5ea2c09a
meryemgkrt <meryemkurt824@gmail.com>
2026-02-15 22:56:47 +0300

    refactor: Update authentication flow and error handling
    - Removed unused dependencies from package.json.
    - Refactored authCallback in authControllers.ts to use atomic upsert for user creation or update.
    - Improved error handling in auth middleware to ensure consistent response structure.
    - Enhanced errorHandler middleware to conditionally include stack trace in development mode.

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -11,13 +11,14 @@ export const protecRoute =[
     async (req: AuthRequest, res: Response, next: NextFunction) => {
         try {
             const {userId:clerkId}= getAuth(req);
-            if (!clerkId) return res.status(401).json({ message: 'Unauthorized' });
+            //if (!clerkId) return res.status(401).json({ message: 'Unauthorized' });
             const user = await User.findOne({ clerkId });
             if (!user) return res.status(404).json({ message: 'User not found' });
             req.userId = user._id.toString();
             next();
         } catch (error) {
-            res.status(500).json({ message: 'Internal server error' });
+            res.status(500);
+            next(error);
         }
     }
 ]
--- backend/src/middleware/errorHandle.ts
+++ backend/src/middleware/errorHandle.ts
@@ -1,12 +1,13 @@
-import type{ Request, Response , NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
 
 
-export const errorHandler = (err:Error, _req: Request, res: Response, _next: NextFunction) => {
+export const errorHandler = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
 
     console.error("errorHandler:", err.message);
 
-    res.status(500).json({ message: "Internal server error",
+    res.status(500).json({
+        message: "Internal server error",
         ...(process.env.NODE_ENV === "development" ? { stack: err.stack } : {})
-     });
+    });
 
 }
