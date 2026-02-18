15151a6b11e13a7f35b2649d2c15664e65ffbe59
meryemgkrt <meryemkurt824@gmail.com>
2026-02-18 23:51:25 +0300

    feat: complete route implementations for mobile application

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,10 +1,15 @@
 
 import { Router } from "express";
+import { protecRoute } from "../middleware/auth";
+import { getChats, getOrCreateChat } from "../controllers/chatControllers";
 
 
 const router = Router();
 
+router.use(protecRoute);
 
+router.get("/", protecRoute, getChats);
+router.post("/", protecRoute, getOrCreateChat);
 
 export default router;
 
--- backend/src/routes/messageRoutes.ts
+++ backend/src/routes/messageRoutes.ts
@@ -1,6 +1,8 @@
 import {Router} from "express";
+import { protecRoute } from "../middleware/auth";
+import { getMesseges } from "../controllers/messageControllers";
 
 const router = Router();
 
-
+router.get("/chat/:chatId", protecRoute, getMesseges )
 export default router;
