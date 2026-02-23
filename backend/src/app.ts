6036caad8dddf3d8ac2846c2e53aae633835ff02
meryemgkrt <meryemkurt824@gmail.com>
2026-02-23 23:49:23 +0300

    Dockerfile add

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import { clerkMiddleware } from '@clerk/express'
 import authRoutes from "./routes/authRoutes";
 import chatRoutes from "./routes/chatRoutes";
@@ -24,6 +25,14 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 app.use(errorHandler);
+ 
+//serve static files in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../web/dist")));
 
+    app.get("/{*any}", (_,res)=>{
+        res.sendFile(path.join(__dirname, "../../web/dist/index.html"));
+    })
+}
 
 export default app;
